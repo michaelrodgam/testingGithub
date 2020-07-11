67d672ae5e666edd2ee7899090b462d40d630c6f
Michael Rodríguez <66528597+michaelrodgam@users.noreply.github.com>
2020-07-11 14:14:13 -0600

    Car.py
    Car class

---
+++ Car.py
@@ -0,0 +1,35 @@
+class Car:
+
+	hasEngine = false
+	engine = "none"
+	modelYear = 0
+	modelName = "none"
+	type = 0
+	typeNames = ["Undefined", "Sedan", "Pickup", "Van", "Monster Truck"]
+	fuelTankLevel = 0
+	damage = 0
+	tiresUse = 0
+	
+	def _init_(aModel, aEngine, aType):
+		self.engine = aEngine
+		self.model = aModel
+		self.type = aType
+	
+	def getName()
+	
+	def getEngine()
+	
+	def getType()
+	
+	def changeEngine()
+	
+	def changeTires()
+	
+	def modifyType()
+	
+	def refuel()
+	
+	def runOneMile()
+	
+	def preformTest()
+	
