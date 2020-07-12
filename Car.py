4774206e377648159e0a3fb49278a7e1ad0770b3
Michael Rodríguez <66528597+michaelrodgam@users.noreply.github.com>
2020-07-12 11:52:30 -0600

    CarClass is ready
    The class car is now ready to use.

--- Car.py
+++ Car.py
@@ -1,35 +1,84 @@
 class Car:
-
-	hasEngine = false
-	engine = "none"
-	modelYear = 0
-	modelName = "none"
-	type = 0
 	typeNames = ["Undefined", "Sedan", "Pickup", "Van", "Monster Truck"]
-	fuelTankLevel = 0
-	damage = 0
-	tiresUse = 0
-	
-	def _init_(aModel, aEngine, aType):
+
+	def __init__(self, aModel, aEngine, aType, aColor):
 		self.engine = aEngine
 		self.model = aModel
 		self.type = aType
-	
-	def getName()
-	
-	def getEngine()
-	
-	def getType()
-	
-	def changeEngine()
-	
-	def changeTires()
-	
-	def modifyType()
-	
-	def refuel()
-	
-	def runOneMile()
-	
-	def preformTest()
-	
+		self.color = aColor
+		self.tankSize = 20 + (15 * aType)
+		self.fuelTankLevel = 0
+		self.damage = 0
+		self.tiresUse = 0
+
+	def getModel(self):
+		return self.model
+
+	def getEngine(self):
+		return self.engine
+
+	def getType(self):
+		return self.typeNames[self.type]
+
+	def getColor(self):
+		return self.color
+
+#this method is for change the engine, because you want more hp.
+	def changeEngine(self, newEngine):
+		oldEngine = self.engine
+		self.engine = newEngine
+		print("The old " + oldEngine + " engine has been succesfully changed for a " + self.engine + " engine.")
+
+#the tires can get a bad shape if you use it to much, buy a new ones and use this for install it.
+	def changeTires(self):
+		self.tiresUse = 0
+		print("The car have new tires now.")
+
+#Change your Sedan for a Monster truck.
+	def modifyCarType(self, newType):
+		oldType = self.type
+		self.type = newType
+		self.tankSize = 20 + (15 * self.aType)
+		print("The car has been converted from a " + self.typeNames[oldType] + " to a " + self.typeNames[self.type])
+
+#Ok, how much gas you need for a road trip? Well, depends right. Fill the tank here.
+	def refuel(self):
+		self.fuelTankLevel = 100
+		print("The car's tank has been filled.")
+
+#Let's try the power of my new car. Toreto will see the red lights this time.
+#You can run some laps, using the multipler parameter.
+	def runOneMile(self, multipler):
+		counter = 1
+		while(multipler > 0):
+			if(self.fuelTankLevel > 0 and self.damage < 100 and self.tiresUse < 100):
+				self.fuelTankLevel -= 5
+				self.tiresUse -= 2
+				print("As far as you can see the car perform well on the " + str(counter) + " mile.\n")
+			else:
+				self.damage += 5
+				multipler = 0
+				print("The car can't complete the mile. Something is wrong.")
+				print("\nSome parts of the car are damaged.")
+			multipler -= 1
+
+#Take a look of the car specs.
+	def CarSpecs(self):
+		return "Model: " + self.getModel() + "\nEngine: " + self.getEngine() + "\nType: " + self.getType() + "\nColor: " + self.getColor()
+
+#This is for detect if the car is ready for the run.
+	def carStatus(self):
+		return "Fuel: "+ str(self.fuelTankLevel) + "%" + "\nDamage: "+ str(self.damage) + "%" + "\nTires use: "+ str(self.tiresUse) + "%"
+
+#This will be sometime later on, like a comparison with others cars.
+	def getPreformTest():
+		return "The car preform well on the test."
+
+#testing the car class, delete this later on. Please!
+car1 = Car("Subaru Impreza 97", "v6", 1, "blue")
+print(car1.CarSpecs())
+print(car1.carStatus())
+car1.refuel()
+car1.changeTires()
+print(car1.carStatus())
+car1.runOneMile(4)
